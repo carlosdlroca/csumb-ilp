834af8f8d017723e71922034ef72a60e880357d9
Carlos De La Roca <carlosdlroca80@gmail.com>
2021-02-15 16:49:14 -0800

    add custom html

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,72 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Carlos De La Roca | ILP</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <main className={styles.main}>
+                <img
+                    src='/csumb-logo-blue.svg'
+                    alt='CSUMB Logo'
+                    className={styles.Logo}
+                />
+                <h1 className={styles.title}>Carlos De La Roca</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+                <p className={styles.description}>
+                    ILP Portfolio Website Shell
+                </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                <div className={styles.grid}>
+                    <Link href='/courses'>
+                        <a className={styles.card}>
+                            <h3>Courses &rarr;</h3>
+                            <p>
+                                Find in-depth information about the courses I
+                                have taken in my CS degree program
+                            </p>
+                        </a>
+                    </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                    <a
+                        href='https://catalog.csumb.edu/preview_program.php?catoid=2&poid=421&returnto=107'
+                        className={styles.card}
+                    >
+                        <h3>Program&rarr;</h3>
+                        <p>
+                            Learn about the Computer Science, BS Online Degree
+                            Completion Program
+                        </p>
+                    </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                    <a
+                        href='https://csumb-carlos.blogspot.com/'
+                        className={styles.card}
+                    >
+                        <h3>CS Blog &rarr;</h3>
+                        <p>
+                            Join me and read about my journey through the CS
+                            Degree program
+                        </p>
+                    </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                    <a href='https://carlosdlr.me' className={styles.card}>
+                        <h3>Portfolio &rarr;</h3>
+                        <p>
+                            Visit my Online Portfolio and check out my personal
+                            projects{" "}
+                            <span className={styles.code}>
+                                site a work in progress
+                            </span>
+                        </p>
+                    </a>
+                </div>
+            </main>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    );
 }
